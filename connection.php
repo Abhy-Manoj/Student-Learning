7b7d897653cfe6142cc28045a47274bc192772dc
Abhy <69760673+Abhy-Manoj@users.noreply.github.com>
2023-06-06 23:23:12 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,8 @@
 <?php
-session_start();
-$con=mysqli_connect("localhost","root","","student_learning");
+$servername = "containers-us-west-59.railway.app";
+;$username = "root";
+$password = "g9OoC8Ti9EHcZo0Fl0mO";
+$dbname = "railway";
+$port = 5511;
+$con = mysqli_connect($servername, $username, $password, $dbname,$port);
 ?>
