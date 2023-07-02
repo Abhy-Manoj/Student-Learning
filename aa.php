97e45ff256adc01b07fc31f9347a54d3c7d6bd1f
Abhy-Manoj <abhy2817@gmail.com>
2023-07-02 20:57:08 +0530

    ignore

--- aa.php
+++ aa.php
@@ -20,12 +20,6 @@ $username = "username";
 $password = "password";
 $dbname = "mydb";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$servername = "containers-us-west-59.railway.app";
-$username = "root";
-$password = "g9OoC8Ti9EHcZo0Fl0mO";
-$dbname = "railway";
-$port = 5511;
-$conn = mysqli_connect($servername, $username, $password, $dbname,$port);
 
 // Check if the connection was successful
 if (!$conn) {
--- connection.php
+++ connection.php
@@ -1,9 +1,4 @@
 <?php
-$servername = "containers-us-west-59.railway.app";
-;$username = "root";
-$username = "root";
-$password = "g9OoC8Ti9EHcZo0Fl0mO";
-$dbname = "railway";
-$port = 5511;
-$con = mysqli_connect($servername, $username, $password, $dbname,$port);
+session_start();
+$con=mysqli_connect("localhost","root","","student_learning");
 ?>
